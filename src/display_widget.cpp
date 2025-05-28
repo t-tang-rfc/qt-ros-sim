a96f281eb1e4487e78304da5c380c7bb6d6790f9
t-tang-rfc <156632897+t-tang-rfc@users.noreply.github.com>
2025-05-28 18:13:21 +0900

    Add a simple WASD keyboard controller

--- src/display_widget.cpp
+++ src/display_widget.cpp
@@ -3,6 +3,10 @@
  * 
  * @brief: implementation of the DisplayWidget class
  * 
+ * @author:
+ * - madpang
+ * - t-tang-rfc
+ * 
  * @date:
  * - created on 2025-05-22
  * - updated on 2025-05-26
@@ -22,14 +26,56 @@ DisplayWidget::DisplayWidget(QWidget* parent)
 	setResizeMode(QQuickWidget::SizeRootObjectToView);
 
 	// Create RobotControllerDelegate as a child
-	auto controller_delegate = new RobotControllerDelegate(this);
+	m_controller = new RobotControllerDelegate(this);
 
 	// Expose to QML as 'controller'
-	rootContext()->setContextProperty(QStringLiteral("controller"), controller_delegate);
+	rootContext()->setContextProperty(QStringLiteral("controller"), m_controller);
+
+	// Connect signals to controller slots
+	connect(this, &DisplayWidget::setPosX, m_controller, &RobotControllerDelegate::setPosX);
+	connect(this, &DisplayWidget::setPosY, m_controller, &RobotControllerDelegate::setPosY);
+	connect(this, &DisplayWidget::setPosZ, m_controller, &RobotControllerDelegate::setPosZ);
 
 	setSource(QUrl("qrc:/qml/Display.qml"));
 }
 
 DisplayWidget::~DisplayWidget() = default;
+
+void DisplayWidget::keyPressEvent(QKeyEvent* event)
+{
+	qreal x = m_controller->posX();
+	qreal y = m_controller->posY();
+	qreal z = m_controller->posZ();
+
+	switch (event->key()) {
+		case Qt::Key_W:
+			x += m_moveStep;
+			emit setPosX(x);
+			break;
+		case Qt::Key_S:
+			x -= m_moveStep;
+			emit setPosX(x);
+			break;
+		case Qt::Key_A:
+			z -= m_moveStep;
+			emit setPosZ(z);
+			break;
+		case Qt::Key_D:
+			z += m_moveStep;
+			emit setPosZ(z);
+			break;
+		case Qt::Key_Up:
+			y += m_moveStep;
+			emit setPosY(y);
+			break;
+		case Qt::Key_Down:
+			y -= m_moveStep;
+			emit setPosY(y);
+			break;
+		default:
+			QQuickWidget::keyPressEvent(event);
+			break;
+	}
+}
 
 } // namespace rf
--- src/display_widget.hpp
+++ src/display_widget.hpp
@@ -3,7 +3,9 @@
  * 
  * @brief: A widget class for displaying the 3D scene with the robot
  * 
- * @author: madpang
+ * @author:
+ * - madpang
+ * - t-tang-rfc
  *
  * @date:
  * - created on 2025-05-22
@@ -14,15 +16,29 @@
 #define DISPLAY_WIDGET_HPP
 
 #include <QQuickWidget>
+#include "robot_controller_delegate.hpp"
 
 namespace rf {
 
 class DisplayWidget : public QQuickWidget
 {
 	Q_OBJECT
-public:
-	explicit DisplayWidget(QWidget* parent = nullptr);
-	~DisplayWidget();
+
+	public:
+		explicit DisplayWidget(QWidget* parent = nullptr);
+		~DisplayWidget();
+
+	signals:
+		void setPosX(qreal x);
+		void setPosY(qreal y);
+		void setPosZ(qreal z);
+
+	protected:
+		void keyPressEvent(QKeyEvent* event) override;
+
+	private:
+		RobotControllerDelegate* m_controller = nullptr;
+		const qreal m_moveStep = 5.0;
 };
 
 } // namespace rf
